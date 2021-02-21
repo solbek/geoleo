d587565f38e9859ed9683da7dc6fac374b0170f3
solveig <solveigt_bekken@live.no>
2021-02-21 12:37:30 +0100

    Update db.js
    funker det?

--- js/db.js
+++ js/db.js
@@ -1,5 +1,4 @@
 /*const pgp = require('pg-promise')();
-
 //db connect string
 const db = pgp('postgres://postgres:aa@localhost:5432/geoleo');
 
@@ -9,11 +8,7 @@ const ps = {}; //object that contains the statements
 
 ps.listAll = new PrpSt('listAll', 'SELECT * FROM travelsview');
 ps.addTravel = new PrpSt('addTravel', 'INSERT INTO travels VALUES(DEFAULT, $1, $2, $3, $4)');
-ps.delTravel = new PrpSt('delTravel', 'DELETE FROM travels WHERE id=$1 RETURNING *');
-
-//export module
-module.exports.db = db; //db connection
-module.exports.ps = ps; //prepared sql statements*/
+ps.delTravel = new PrpSt('delTravel', 'DELETE FROM travels WHERE id=$1 RETURNING *');*/
 
 const pg = require("pg");
 const { Client } = require('pg');
@@ -73,5 +68,5 @@ db.delete = async function (query) {
 db.update = async function (query) {
   return await runQuery(query);
 }
-
+//export module - db connection
 module.exports = db;
